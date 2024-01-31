43ac5dfb5ddbcb7529468dee174d9ccec5886af0
kaluczadzsi <kaluczadzsi@gmail.com>
2024-01-31 20:06:32 +0100

    array types

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -6,6 +6,13 @@
 const person = {
   name: 'Gabor',
   age: 30,
+  hobbies: ['Sports', 'Cooking'],
 };
 
+let favoriteActivities: string[];
+
 console.log(person);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
