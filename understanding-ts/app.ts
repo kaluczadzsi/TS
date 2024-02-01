593eba5398714655966efaf409f5c1405b94bbc7
kaluczadzsi <kaluczadzsi@gmail.com>
2024-02-01 19:47:10 +0100

    working with enums

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -3,16 +3,21 @@
 //   age: 30,
 // };
 
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
-  name: 'Gabor',
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
+  name: 'Max',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author'],
+  role: Role.AUTHOR,
 };
 
 // person.role.push('admin');
@@ -26,4 +31,8 @@ console.log(person);
 
 for (const hobby of person.hobbies) {
   console.log(hobby);
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is author');
 }
