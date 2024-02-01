be484337b6c0c867afd8407f4a370a2c4ba96026
kaluczadzsi <kaluczadzsi@gmail.com>
2024-02-01 17:03:52 +0100

    working with tuples

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -3,11 +3,22 @@
 //   age: 30,
 // };
 
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'Gabor',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
 };
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user'];
 
 let favoriteActivities: string[];
 
