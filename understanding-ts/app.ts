db04e34dfa5bfdf105806b3aeaa4a6f881febadc
kaluczadzsi <kaluczadzsi@gmail.com>
2024-02-07 20:53:09 +0100

    functions as types

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -8,4 +8,11 @@ function printResult(num: number): undefined {
 
 printResult(add(5, 12));
 
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+
+console.log(combineValues(8, 8));
+
 // let someValue: undefined;
