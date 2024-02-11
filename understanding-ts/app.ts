c6bc526d15348252b2127906ea0e3855dfc3a3d2
kaluczadzsi <kaluczadzsi@gmail.com>
2024-02-11 19:04:12 +0100

    using watch mode

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,15 +1,3 @@
-let userInput: unknown;
-let userName: string;
+const userName = 'Max';
 
-userInput = 5;
-userInput = 'Max';
-if (typeof userInput === 'string') {
-  userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-}
-
-const result = generateError('An error occurred!', 500);
-console.log(result);
+console.log(userName);
