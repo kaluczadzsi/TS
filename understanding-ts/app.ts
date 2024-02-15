13e103cb328f26f1ffa3e222ae818bbdac3ec8ef
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-02-15 20:51:50 +0100

    old files deleted

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,6 +1,14 @@
+let appId = 'abc';
 const button = document.querySelector('button')!;
 
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+}
+
 function clickHandler(message: string) {
+  let userName = 'Gabor';
   console.log('Clicked! ' + message);
 }
 
