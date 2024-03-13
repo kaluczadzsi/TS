ae1d66ecbf04e89d9d0b8203f9dca8c4e8430e0f
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-03-13 18:17:16 +0100

    using interfaces with classes

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,8 +1,13 @@
-var user1 = {
-    name: 'Tomas',
-    age: 29,
-    greet: function (phrase) {
-        console.log(phrase + ' ' + this.name);
-    },
-};
+var Person = /** @class */ (function () {
+    function Person(name) {
+        this.name = name;
+    }
+    Person.prototype.greet = function (phrase) {
+        console.log("".concat(phrase, " ").concat(this.name));
+    };
+    return Person;
+}());
+var user1;
+user1 = new Person('John Doe');
+console.log(user1);
 user1.greet('Hi there - I am');
--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,16 +1,23 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
-
   greet(phrase: string): void;
 }
 
-let user1: Person = {
-  name: 'Tomas',
-  age: 29,
-  greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);
-  },
-};
+class Person implements Greetable {
+  name: string;
 
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  greet(phrase: string): void {
+    console.log(`${phrase} ${this.name}`);
+  }
+}
+
+let user1: Greetable;
+
+user1 = new Person('John Doe');
+
+console.log(user1);
 user1.greet('Hi there - I am');
