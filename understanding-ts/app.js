caf4c6a02f2317bf067fed22fd9d3b0c544b01f7
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-03-16 12:39:59 +0100

    index properties

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -68,3 +68,7 @@ moveAnimal({ type: 'bird', flyingSpeed: 300 });
 // );
 var userInputElement = document.getElementById('user-input');
 userInputElement.value = 'Hi there';
+var errorBag = {
+    email: 'Not a valid email',
+    username: 'Must start with a capital character',
+};
