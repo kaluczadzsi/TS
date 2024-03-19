e106515b60701d955081440fa9e3a9f15bb7d0be
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-03-19 18:21:50 +0100

    generic classes

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -17,19 +17,105 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 const mergedObj = merge({ name: 'Max' }, { age: 30 });
-*/
-function countAndDescribe(element) {
-    var descriptionText = 'Got no value.';
-    if (element.length === 1) {
-        descriptionText = 'Got 1 element';
-    }
-    else if (element.length > 1) {
-        descriptionText = 'Got ' + element.length + ' elements.';
-    }
-    return [element, descriptionText];
+
+type Lengthy = {
+  length: number;
+};
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element';
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements.';
+  }
+
+  return [element, descriptionText];
 }
+
 console.log(countAndDescribe(['Sports', 'Cooking']));
-function extractAndConvert(obj, key) {
-    return obj[key];
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return obj[key];
 }
+
 console.log(extractAndConvert({ name: 'Mike' }, 'name'));
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Mike');
+textStorage.addItem('Yorick');
+textStorage.removeItem('Mike');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(2);
+numberStorage.addItem(7);
+numberStorage.addItem(99);
+numberStorage.removeItem(2);
+console.log(numberStorage.getItems());
+
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({ name: 'Mike' });
+// objStorage.addItem({ name: 'Jolene' });
+
+// objStorage.removeItem({ name: 'Mike' });
+// console.log(objStorage.getItems());
+
+type User = {
+  id: number;
+  name: string;
+};
+
+class UserStorage<T extends User> {
+  private data: T[] = [];
+
+  addUser(user: T) {
+    this.data.push(user);
+  }
+
+  removeUser(id: number) {
+    this.data = this.data.filter((user) => user.id != id);
+  }
+
+  getUsers() {
+    console.log(this.data);
+  }
+}
+
+const userStorage = new UserStorage<User>();
+
+const getRandomId = () => {
+  return Math.round(Math.random() * 9 + 1);
+};
+
+userStorage.addUser({ id: 1, name: 'John' });
+userStorage.addUser({ id: 2, name: 'Tomas' });
+userStorage.addUser({ id: 3, name: 'Grace' });
+
+userStorage.getUsers();
+userStorage.removeUser(3);
+userStorage.getUsers();
+*/
--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -17,7 +17,6 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 const mergedObj = merge({ name: 'Max' }, { age: 30 });
-*/
 
 type Lengthy = {
   length: number;
@@ -44,3 +43,79 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 console.log(extractAndConvert({ name: 'Mike' }, 'name'));
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Mike');
+textStorage.addItem('Yorick');
+textStorage.removeItem('Mike');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(2);
+numberStorage.addItem(7);
+numberStorage.addItem(99);
+numberStorage.removeItem(2);
+console.log(numberStorage.getItems());
+
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({ name: 'Mike' });
+// objStorage.addItem({ name: 'Jolene' });
+
+// objStorage.removeItem({ name: 'Mike' });
+// console.log(objStorage.getItems());
+
+type User = {
+  id: number;
+  name: string;
+};
+
+class UserStorage<T extends User> {
+  private data: T[] = [];
+
+  addUser(user: T) {
+    this.data.push(user);
+  }
+
+  removeUser(id: number) {
+    this.data = this.data.filter((user) => user.id != id);
+  }
+
+  getUsers() {
+    console.log(this.data);
+  }
+}
+
+const userStorage = new UserStorage<User>();
+
+const getRandomId = () => {
+  return Math.round(Math.random() * 9 + 1);
+};
+
+userStorage.addUser({ id: 1, name: 'John' });
+userStorage.addUser({ id: 2, name: 'Tomas' });
+userStorage.addUser({ id: 3, name: 'Grace' });
+
+userStorage.getUsers();
+userStorage.removeUser(3);
+userStorage.getUsers();
+*/
